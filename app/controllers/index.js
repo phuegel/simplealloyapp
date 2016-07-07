4d5f19a298e671901860f44ece941691df2a4b9f
Patrick Huegel <phuegel@PHuegels-MacBook-Pro.local>
2016-07-07 17:35:47 -0400

    Multiple buttons with vertical menu items layout

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,23 @@
-$.open_button.addEventListener("click", function(event) {
+
+$.open_button_1.addEventListener("click", function(event) {
 	console.debug("is open_button click event handler");
 	
-	$.tab_1.open($.child_window);
+	$.tab_1.open($.child_window_1);
 });
 
+$.open_button_2.addEventListener("click", function(event) {
+	console.debug("is open_button click event handler");
+	
+	$.tab_1.open($.child_window_2);
+	
+});$.open_button_3.addEventListener("click", function(event) {
+	console.debug("is open_button click event handler");
+	
+	$.tab_1.open($.child_window_3);
+	
+});$.open_button_4.addEventListener("click", function(event) {
+	console.debug("is open_button click event handler");
+	
+	$.tab_1.open($.child_window_4);
+});
 $.index.open();
